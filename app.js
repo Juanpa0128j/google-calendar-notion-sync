f3ec4c51f85b01aac459a4798056ad9ee3a93335
Glitch (node-red-glitch) <none>
2018-03-26 07:56:40 +0000

    💁🍅 Checkpoint ./app.js:409557/376 ./.env:409557/11

--- app.js
+++ app.js
@@ -22,11 +22,13 @@ const {
   VIEW_WITHOUT_LOGIN,
 } = process.env
 
+const baseURL = `https://${PROJECT_DOMAIN}.glitch.me/`
+
 const settings = {
   adminAuth: githubAuth({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    baseURL: 'https://' + PROJECT_DOMAIN + '.glitch.me/',
+    baseURL,
     users: [{ username: GITHUB_USERNAME, permissions: ['*'] }],
   }),
   httpNodeCors: {
@@ -42,19 +44,21 @@ const settings = {
 if (VIEW_WITHOUT_LOGIN && JSON.parse(VIEW_WITHOUT_LOGIN))
   settings.adminAuth.default = { permissions: 'read' }
 
-const keepalive = () => {
-  if (PROJECT_DOMAIN) request(
-    { url: `http://${PROJECT_DOMAIN}.glitch.me/glitch-alive` },
-    () => setTimeout(() => keepalive(), 55000)
-  )
-}
+const keepalive = () => PROJECT_DOMAIN && request(
+  { url: `${baseURL}glitch-alive` },
+  () => setTimeout(keepalive, 55000)
+)
 
 RED.init(server, settings)
 RED.start()
 
 app.use('/', express.static('public'))
-app.get('/glitch-alive', (req, res) => res.json({ isAlive: true }))
+// app.get('/glitch-alive', (req, res) => res.json({ isAlive: true }))
+app.get('/glitch-alive', (req, res) => {
+  console.log('alive')
+  res.json({ isAlive: true })
+})
 app.use(settings.httpAdminRoot, RED.httpAdmin)
 app.use(settings.httpNodeRoot, RED.httpNode)
 
-server.listen(PORT, KEEP_ALIVE && JSON.parse(KEEP_ALIVE) ? keepalive : () => true)
+server.listen(PORT, KEEP_ALIVE && JSON.parse(KEEP_ALIVE) ? keepalive : () => void 0)
