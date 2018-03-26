e83b9f595a940233268637daa328f5343e30a0cd
Glitch (node-red-glitch) <none>
2018-03-26 06:40:32 +0000

    ☕️🚂 Checkpoint ./package.json:409557/988 ./app.js:409557/8939 ./.env:409557/621 ./README.md:409557/478

--- README.md
+++ README.md
@@ -1,18 +1,4 @@
-# My cool website
+# node-red-glitch - A minimal node-red installation running on express for Glitch
 
-This file describes your project to the community. What's your cool website about? What makes it special?
-
-ヽ(๏∀๏ )ﾉ
-
-## ← index.html
-
-Where you'll write the content of your website. 
-
-## ← styles.css
-
-CSS files add styling rules to your content
-
-## ← script.js
-
-If you're feeling fancy you can add interactivity to your site with Javascript
+## Getting started
 
--- app.js
+++ app.js
@@ -2,10 +2,9 @@ require('dotenv').config()
 
 const http = require('http')
 const express = require('express')
+const request = require('request')
 const RED = require('node-red')
 const githubAuth = require('node-red-auth-github')
-
-const keepalive = require('./keepalive')
 
 // Create an Express app
 const app = express()
@@ -15,24 +14,12 @@ const server = http.createServer(app)
 
 const {
   PORT,
-  ADMIN_USERNAME,
-  ADMIN_PASSWORD_HASH,
+  GITHUB_USERNAME,
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
   PROJECT_DOMAIN,
   KEEP_ALIVE,
 } = process.env
-
-const adminAuthStatic = {
-  type: 'credentials',
-  users: [
-    {
-      username: ADMIN_USERNAME,
-      password: ADMIN_PASSWORD_HASH,
-      permissions: '*',
-    },
-  ],
-}
 
 // Create the settings object - see default settings.js file for other options
 const settings = {
@@ -40,7 +27,7 @@ const settings = {
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
     baseURL: 'https://' + PROJECT_DOMAIN + '.glitch.me/',
-    users: [{ username: ADMIN_USERNAME, permissions: ['*'] }],
+    users: [{ username: GITHUB_USERNAME, permissions: ['*'] }],
     default: {
       permissions: 'read',
     },
@@ -56,21 +43,27 @@ const settings = {
   functionGlobalContext: {}, // enables global context
 }
 
+const keepalive = () => {
+  const reqOpts = {
+    url: 'http://' + process.env.PROJECT_DOMAIN + '.glitch.me/its-alive',
+  }
+  const keepalive = () =>
+    request(reqOpts, () => setTimeout(() => keepalive(), 55000))
+  // if this is running on Glitch, call self every 55 secs
+  if (process.env.PROJECT_DOMAIN) keepalive()
+}
+
 // Initialise the runtime with a server and settings
 RED.init(server, settings)
-
 // Add a simple route for static content served from 'public'
 app.use('/', express.static('public'))
 // Glitch keepalive
 app.get('/its-alive', (req, res) => res.json({ isAlive: true }))
-
 // Serve the editor UI from /red
 app.use(settings.httpAdminRoot, RED.httpAdmin)
-
 // Serve the http nodes UI from /api
 app.use(settings.httpNodeRoot, RED.httpNode)
-
-server.listen(PORT, KEEP_ALIVE ? keepalive : () => true )
-
 // Start the runtime
 RED.start()
+// start the server
+server.listen(PORT, KEEP_ALIVE ? keepalive : () => true)
